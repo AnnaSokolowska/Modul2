abc7f4bd21465111588b476f67855a2c87f14ad7
Anna Sokolowska <milanka.anka@gmail.com>
2022-08-06 14:52:03 +0200

    New work

---
+++ todo-app/modules/createElement.js
@@ -0,0 +1,9 @@
+
+
+const createContainer = () => {
+  const container = document.querySelector('.app-container');
+  container.classname = 'app-container vh-100 w-100 d-flex align-items-center justify-content-center flex-column';
+  return container;
+};
+createContainer();
+
